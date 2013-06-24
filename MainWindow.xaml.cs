ed764c5cfedfc9e2a116238f95bc741be46c1428
ddreier <daniel@ddreier.com>
2013-06-24 02:35:52 -0500

    Initial Commit!

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.MemoryMappedFiles;
+using System.Text.RegularExpressions;
+using System.Windows.Threading;
+
+namespace GW2MumbleLinkTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MumbleLink data = new MumbleLink();
+        
+        public MainWindow()
+        {
+            // Sizeof(Link) is 10580 as reported by the python app
+            InitializeComponent();
+            
+            try
+            {
+                MemoryMappedFile.CreateNew("MumbleLink", 10580);
+                
+                /*using (var mmf = MemoryMappedFile.OpenExisting("MumbleLink"))
+                {
+                    using (var accessor = mmf.CreateViewAccessor())
+                    {
+                        uint[] value = new uint[256];
+
+                        //name (program name, Guild Wars 2) is at location 44
+                        //identity (character name) is at 592
+                        //context (uint) at 1108
+                        accessor.ReadArray<uint>(1108, value, 0, 10);
+
+                        foreach (uint c in value)
+                        {
+                            MessageBox.Show(c.ToString());
+                        }
+                    }
+                }*/
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Trouble creating the Mumble Link MMF");
+
+                Application.Current.Shutdown();
+            }
+
+            System.Windows.Threading.DispatcherTimer refreshTimer = new System.Windows.Threading.DispatcherTimer();
+            refreshTimer.Tick += refreshTimer_Tick;
+            refreshTimer.Interval = new TimeSpan(TimeSpan.TicksPerSecond / 30);
+            refreshTimer.Start();
+        }
+
+        void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var mmf = MemoryMappedFile.OpenExisting("MumbleLink"))
+                {
+                    using (var accessor = mmf.CreateViewAccessor())
+                    {
+                        float[] avPos = new float[3];
+                        accessor.ReadArray<float>(2 * sizeof(uint), avPos, 0, 3);
+                        tbXPos.Text = avPos[0].ToString();
+                        tbYPos.Text = avPos[1].ToString();
+                        tbZPos.Text = avPos[2].ToString();
+
+                        float[] avFront = new float[3];
+                        accessor.ReadArray<float>(2 * sizeof(uint) + 3 * sizeof(float), avFront, 0, 3);
+                        tbXAvFront.Text = avFront[0].ToString();
+                        tbYAvFront.Text = avFront[1].ToString();
+                        tbZAvFront.Text = avFront[2].ToString();
+
+                        char[] ident = new char[256];
+                        accessor.ReadArray<char>(592, ident, 0, 256);
+                        string charName = new string(ident);
+                        Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+                        charName = rgx.Replace(charName, "");
+                        tbCharName.Text = charName;
+
+                        uint[] context = new uint[10];
+                        accessor.ReadArray<uint>(1108, context, 0, 10);
+                        tbMapID.Text = context[7].ToString();
+                        tbWorldID.Text = context[9].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                DataWindow.Background = Brushes.Red;
+                MessageBox.Show("Trouble accessing Mumble Link MMF");
+                ((System.Windows.Threading.DispatcherTimer)sender).Stop();
+            }
+        }
+    }
+}
---
+++ MumbleLink.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2MumbleLinkTesting
+{
+    class MumbleLink
+    {
+        public uint uiVersion { get; set; }
+        public uint uiTick { get; set; }
+        public float[] fAvatarPosition { get; set; }
+        public float[] fAvatarFront { get; set; }
+        public float[] fAvatarTop { get; set; }
+        public char[] name { get; set; }
+        public float[] fCameraPosition { get; set; }
+        public float[] fCameraFront { get; set; }
+        public float[] fCameraTop { get; set; }
+        public char[] identity { get; set; }
+        public uint context_len { get; set; }
+        public char[] context { get; set; }
+        public char[] description { get; set; }
+
+        public MumbleLink() { }
+
+        public MumbleLink(uint uiVersion, uint uiTick, float[] fAvatarPosition, float[] fAvatarFront, float[] fAvatarTop, char[] name,
+                            float[] fCameraPosition, float[] fCameraFront, float[] fCameraTop, char[] identity, uint context_len,
+                            char[] context, char[] description)
+        {
+            this.uiVersion = uiVersion;
+            this.uiTick = uiTick;
+            this.fAvatarPosition = fAvatarPosition;
+            this.fAvatarFront = fAvatarFront;
+            this.fAvatarTop = fAvatarTop;
+            this.name = name;
+            this.fCameraPosition = fCameraPosition;
+            this.fCameraFront = fCameraFront;
+            this.fCameraTop = fCameraTop;
+            this.identity = identity;
+            this.context_len = context_len;
+            this.context = context;
+            this.description = description;
+        }
+    }
+}

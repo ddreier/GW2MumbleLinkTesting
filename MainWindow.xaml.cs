84eadda5789b7bed15255720237330e6d981d43c
ddreier <daniel@ddreier.com>
2013-06-26 12:11:30 -0500

    Removed unused usings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO.MemoryMappedFiles;
-using System.Text.RegularExpressions;
 using System.Windows.Threading;
-using System.Net;
 
 namespace GW2MumbleLinkTesting
 {
